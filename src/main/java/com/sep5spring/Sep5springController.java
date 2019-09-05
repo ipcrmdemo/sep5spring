1eef6eb249baff350e0db8ad1384a84ddca588d4
Matt Cadorette <mcadorette@gmail.com>
2019-09-05 10:56:30 -0400

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/com/sep5spring/Sep5springController.java
@@ -0,0 +1,15 @@
+package com.sep5spring;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+class Sep5springController {
+
+    @GetMapping("/hello/{name}")
+    public String person(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+
+}
---
+++ src/test/java/com/sep5spring/Sep5springApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sep5spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Sep5springApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
